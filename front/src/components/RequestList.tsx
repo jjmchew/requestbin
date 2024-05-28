b256761c8a99e6dbf79711dadffa5c0371d5d588
Ben Boinay <ben.boinay@gmail.com>
2024-05-28 15:48:57 -0400

    change sample data dateTime to date & time

--- front/src/components/RequestList.tsx
+++ front/src/components/RequestList.tsx
@@ -8,7 +8,7 @@ const RequestList = ({ data }: RequestListProps) => {
   let display = data.map(obj => {
     return (
       <div className={classes.rowWrap}>
-        <div className={classes.time}>{obj.dateTime}</div>
+        <div className={classes.date}>{obj.date} {obj.time}</div>
         <div className={classes.method}>{obj.method}</div>
         <div className={classes.path}>{obj.path}</div>
       </div>
